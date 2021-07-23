f14daa592c7fab7065a30c896fb50e9ff0ee2d5c
Yashmita <55138349+yashmita@users.noreply.github.com>
2021-07-24 01:20:21 +0530

    Added logout in upload.js

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -24,3 +24,15 @@ upload.addEventListener('click', function(e){
 	}
 	})
 })
+
+function logOut(){
+	if(github === 1 || googlelogin == 1){
+	  firebase.auth().signOut().then(()=>{
+		alert('User logged out successfully')
+	  }).catch(e=>{
+		console.log(e.message);
+	  })
+	}
+	localStorage.removeItem("userLogin");
+	window.location.replace("../index.html"); 
+  }
